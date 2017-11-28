652ace4f60a0ae1be272c2d94a7592c0195a66ab
Lizhong-Liu <lizhongliu@uchicago.edu>
2017-11-28 12:11:30 -0600

    Lizhong: regression and results

--- data_regression.py
+++ data_regression.py
@@ -3,7 +3,11 @@
 import pandas as pd
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid", rc={'axes.linewidth': 2.5})
+sns.set_context('notebook', font_scale=1.45, rc={"lines.linewidth": 3, "figure.figsize" : (7, 3)})
 
+# From 2004 to 2015.
 df = pd.read_csv('school_merged.csv', sep='\t', low_memory=False)
 df.rename(columns={'FRPM_%': 'FRPM_RATE'}, inplace=True)
 df = df[df['FRPM_RATE'] != 'N / A']
@@ -25,7 +29,8 @@ for y in dependents:
     formula2 = "{} ~ I + FEMALE_RATIO + WHITE_RATIO + FRPM_RATE".format(y)
     ols = smf.ols(formula=formula2, data=treatment)
     model = ols.fit()
-    print(model.summary())
+    result = open("regression_results/df/Reg-treatment-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
 
 # Control group trend.
 control = df[df['D'] == 0]
@@ -33,7 +38,8 @@ for y in dependents:
     formula3 = "{} ~ I + FEMALE_RATIO + WHITE_RATIO + FRPM_RATE".format(y)
     ols = smf.ols(formula=formula3, data=control)
     model = ols.fit()
-    print(model.summary())
+    result = open("regression_results/df/Reg-control-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
 
 # Baseline balance test.
 data = df[df['I'] == 0]
@@ -41,14 +47,16 @@ for c in controls:
     formula = "{} ~ D".format(c)
     ols = smf.ols(formula=formula, data=data)
     model = ols.fit()
-    print(model.summary())
+    result = open("regression_results/df/Reg-baseline-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
 
 # DID analysis.
 for y in dependents:
     formula3 = "{} ~ I + D + D*I + FEMALE_RATIO + WHITE_RATIO + FRPM_RATE".format(y)
     ols = smf.ols(formula=formula3, data=df)
     model = ols.fit()
-    print(model.summary())
+    result = open("regression_results/df/Reg-did-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
 
 # Plot the trend for treatment and control group.
 health_trend_t = df[df['D'] == 1].groupby('YEAR').mean()
@@ -62,3 +70,53 @@ health_trend_c = df[df['D'] == 0].groupby('YEAR').mean()
 for i in dependents:
     health_trend_c['{}'.format(i)].plot()
     plt.show()
+
+# Restrict samples to schools with data from 2011 to 2015.
+df1 = df[df['YEAR'] > 2010]
+dic = {}
+for i in df1['CDS_CODE']:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
+keys = [i for i in dic if dic[i] == 5]
+df1.set_index(['CDS_CODE', 'YEAR'], inplace=True)
+df1 = df1.loc[keys, :]
+
+# Baseline analysis.
+baseline = df1[df1['I'] == 0]
+for c in controls:
+    ols = smf.ols(formula="{} ~ D".format(c), data=baseline)
+    model = ols.fit()
+    result = open("regression_results/df1/Reg-baseline-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
+
+# Health trends of treatment and control group.
+health_trend_t = df1[df1['D'] == 1].groupby('YEAR').mean()
+for i in dependents:
+    health_trend_t['{}'.format(i)].plot()
+    plt.show()
+health_trend_c = df1[df1['D'] == 0].groupby('YEAR').mean()
+for i in dependents:
+    health_trend_c['{}'.format(i)].plot()
+    plt.show()
+
+# regplot.
+for c in controls:
+    plt.clf()
+    ax = sns.regplot(x = "{}".format(c), y = "HEALTH_TOTAL", data = baseline[baseline['D'] == 1], scatter_kws = {"alpha" : 0.1})
+    ax.set(title='{} - TREATMENT GROUP'.format(c))
+    plt.savefig('plots/{}-treatment.pdf'.format(c))
+for c in controls:
+    plt.clf()
+    ax = sns.regplot(x = "{}".format(c), y = "HEALTH_TOTAL", data = baseline[baseline['D'] == 0], scatter_kws = {"alpha" : 0.1})
+    ax.set(title='{} - CONTROL GROUP'.format(c))
+    plt.savefig('plots/{}-control.pdf'.format(c))
+
+# DiD analysis.
+for y in dependents:
+    ols = smf.ols(formula="{} ~ I + D + D*I + FEMALE_RATIO + WHITE_RATIO + FRPM_RATE".format(y),
+                  data=df1)
+    model = ols.fit()
+    result = open("regression_results/df1/Reg-didda-{}.txt".format(y.lower()), "w")
+    result.write(model.summary().as_text())
